987526cc9bdfd2d7c1487c38b612bae2b7e0aed3
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-03 18:08:29 +0200

    entrega1-2

---
+++ entrega1-2.js
@@ -0,0 +1,40 @@
+// nivell 1 - exercici 1
+const sum = () => {return 1 + 14}
+console.log(sum())
+
+//nivell 2 - exercici 1
+let object = (parameter) => parameter
+console.log(object(`Aquesta frase es el meu paràmetre.`))
+
+//nivell 2 - exercici 2
+class Persona {
+    #name
+    constructor (name) {
+        this.#name = name
+    }
+    getName(){
+        return this.#name
+    }
+}
+
+let persona = new Persona ('Àlex')
+console.log(persona.getName())
+
+//nivell 3 - exercici 1
+class Person {
+    constructor (){
+        throw new Error ('You shall not create a class!')
+    }
+}
+
+function createName(name) {
+let nameId = Object.create(Person.prototype)
+nameId.name = name
+return nameId
+}
+
+let alex = createName('Àlex')
+let merche = createName('Merche')
+
+console.log(alex instanceof Person, alex.name);
+console.log(merche instanceof Person, merche.name);
