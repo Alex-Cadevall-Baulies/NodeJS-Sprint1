8b2911059d0237831b838e40ecf31f05a09b086e
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-03 19:15:50 +0200

    exercici2 lvl 1 fet

--- entrega1-4.js
+++ entrega1-4.js
@@ -4,11 +4,11 @@ let employees = [{
 }, {
     id: 2,
     name: 'Bill Gates'
-},{
+}, {
     id: 3,
     name: 'Jeff Bezos'
 }];
- 
+
 let salaries = [{
     id: 1,
     salary: 4000
@@ -23,29 +23,52 @@ let salaries = [{
 //nivell 1 - exercici 1
 
 const chosenID = 3
-const getEmployee = new Promise ((resolve, reject) => {
-    const isIdTrue =  employees.find(employees => employees.id === chosenID)
-    
+const getEmployee = new Promise((resolve, reject) => {
+    const isIdTrue = employees.find(employees => employees.id === chosenID)
+
     if (isIdTrue !== undefined) {
-    const idName = employees.find(employees => employees.id === chosenID)
-    resolve(idName)
+        const idName = employees.find(employees => employees.id === chosenID)
+        resolve(idName)
+    } else {
+        let error = `El ID ${chosenID} no es troba a la nostra base de dades.`
+        reject(error)
     }
-    
-else {
-    let error = `El ID ${chosenID} no es troba a la nostra base de dades.`
-    reject(error)
-}
 })
 
 const getSalary = idName => {
     const idSalary = salaries.find(salaries => salaries.id === idName.id)
     const message = `El salari de ${idName.name} és ${idSalary.salary}€.`
     return Promise.resolve(message)
-    }
+}
 
-setTimeout (() => {
-getEmployee
-    .then(getSalary)
-    .then(resolve => console.log(resolve))
-    .catch(idInformation => console.log(idInformation))
+setTimeout(() => {
+    getEmployee
+        .then(getSalary)
+        .then(resolve => console.log(resolve))
+        .catch(idInformation => console.log(idInformation))
 }, 3000)
+
+//nivell 1 - exercici 2
+
+const isBookClubAvailable = true
+
+const bookClubBooking = new Promise((resolve, reject) => {
+    if (isBookClubAvailable) {
+        const newReservation = {
+            name: 'Bilbo MC Swaggings',
+            genre: 'fashion and design',
+            location: 'right wing'
+        }
+        setTimeout(() => {
+            resolve(newReservation)
+        }, 2000)
+    }
+    else {
+        let error = `Book club is booked at that time`
+        reject(error)
+    }
+})
+
+bookClubBooking
+.then(meetingconfirmation => console.log(meetingconfirmation))
+.catch(error => console.log(error))
