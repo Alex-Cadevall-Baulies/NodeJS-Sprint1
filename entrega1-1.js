1b4de8021c305ae290c1fa1520fffb39223d02c0
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-03 18:09:25 +0200

    entrega1-1

---
+++ entrega1-1.js
@@ -0,0 +1,50 @@
+const firstName = 'Alex'
+const surname = 'Cadevall Baulies'
+const userCode = "ACB"
+
+userNameAndSurname(firstName, surname)
+sayHiMessage(firstName, surname)
+provideUserName(userCode)
+countArray()
+//trobaràs el nivell 3 - exercici 2 al final
+
+//Nivell 1 - exercici 1
+function userNameAndSurname(name, surname) {console.log(name, surname)}
+
+//nivell 2 - exercici 1
+function sayHiMessage (name, surname) {
+    let firstName = name
+    let lastName = surname
+    let message = `Hola ${firstName} ${lastName} espero que tinguis un bon dia.`
+    console.log(message)
+}
+
+//nivell 2 - exercici 2
+function provideUserName (userCode) {return console.log(`El teu nom d'usuari és: ${userCode}.`)}
+
+//nivell 3 - exercici 1
+function countArray(){
+    const numberArray = []
+    let i 
+
+    for (i=0; i<10; i++) {
+        numberArray.push(numberCount())
+    }
+    
+    console.log(numberArray)
+}
+
+function numberCount() {
+    let message = ''
+    let i
+
+    for (i = 0; i < 10; i++) {
+        message += String(i)
+        console.log(message)
+    }
+    return message
+}
+
+//nivell 3 - exercici 2
+const returnUserCode = function (userCode) {return userCode}
+console.log(returnUserCode(userCode))
