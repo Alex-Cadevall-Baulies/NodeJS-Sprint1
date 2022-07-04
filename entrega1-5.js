1293d8dfd169fe0c33ac7c041529ed0fc944caf2
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-04 09:46:47 +0200

    xercici 2 primer intent

--- entrega1-5.js
+++ entrega1-5.js
@@ -7,3 +7,5 @@ const content = 'We did it, they called us fools but we did it!!'
 
 fs.writeFile('entrega1-5.txt', content, err => {if (err) {console.log(err)}})
 
+//nivell 1 - exercici 2
+fs.readFile('entrega1-5.txt', err => {if (err) {console.log(err)}})
