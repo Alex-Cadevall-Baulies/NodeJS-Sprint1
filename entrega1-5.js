3663695e4cbb288d20c3cfccc0755fd64c637520
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-04 11:08:09 +0200

    gzip creat

--- entrega1-5.js
+++ entrega1-5.js
@@ -8,7 +8,16 @@ const content = 'We did it, they called us fools but we did it!!'
 fs.writeFile('entrega1-5.txt', content, err => {if (err) {console.log(err)}})
 
 //nivell 1 - exercici 2
-fs.readFile('entrega1-5.txt', (error, text) => {if (err) {console.log(error)} console.log(text.toString())})
+fs.readFile('entrega1-5.txt', (error, text) => {if (error) {console.log(error)} console.log(text.toString())})
 
 //nivell 1 - exercici 3
+const zlib = require("zlib")
+const inp = fs.createReadStream('entrega1-5.txt')
+const out = fs.createWriteStream('entrega1-5.txt.gz')
+const gzip = zlib.createGzip()
+
+inp.pipe(gzip).pipe(out);
+console.log("Gzip created!")
+/*
 const { spawn } = require('child_process')
+const zipFile =  */
