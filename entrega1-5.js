749f952f8a73c291840af5c714705efcbe7bba9f
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-04 10:01:39 +0200

    lvl1 ex 3

--- entrega1-5.js
+++ entrega1-5.js
@@ -8,4 +8,6 @@ const content = 'We did it, they called us fools but we did it!!'
 fs.writeFile('entrega1-5.txt', content, err => {if (err) {console.log(err)}})
 
 //nivell 1 - exercici 2
-fs.readFile('entrega1-5.txt', err => {if (err) {console.log(err)}})
+fs.readFile('entrega1-5.txt', (error, text) => {if (err) {console.log(error)} console.log(text.toString())})
+
+//nivell 1 - exercici 3
