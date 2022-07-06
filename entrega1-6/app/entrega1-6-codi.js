8c28bb2f4f896775fa93530bbf41b0626fb345aa
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-06 09:42:23 +0200

    entrega1-6

---
+++ entrega1-6/app/entrega1-6-codi.js
@@ -0,0 +1,9 @@
+const number1 = 2
+const number2 = 4
+
+const sum = function sum() {return console.log(number1+number2)}
+const substract = function substract() {return console.log(number1-number2)}
+const multiply = function multiply() {return console.log(number1*number2)}
+const divide = function divide() {return console.log(number1/number2)}
+
+
