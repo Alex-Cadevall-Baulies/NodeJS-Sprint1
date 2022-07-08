0f3fa1ea5abc18c483445e6b547a3ec6a5e606b1
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-08 18:16:09 +0200

    changes

--- entrega1-3.js
+++ entrega1-3.js
@@ -1,10 +1,6 @@
 //nivell 1 - exercici 1
 
-<<<<<<< HEAD
 const isWaterType = 'Squirtel'
-=======
-const isWaterType = 'Mudkip'
->>>>>>> e83f90baceb64fe43389fff221e918253da20055
 const waterStarter = new Promise ((resolve, reject) => {
     if(isWaterType === !'Squirtel' || !'Totodile' || !'Mudkip') {
         let message = `${isWaterType} no es tipus aigua!`
