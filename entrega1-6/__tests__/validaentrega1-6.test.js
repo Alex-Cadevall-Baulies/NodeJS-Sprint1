ee014b7d6e1c4fc43554e71f561580981f0bf300
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 11:08:27 +0200

    sum test

--- entrega1-6/__tests__/validaentrega1-6.test.js
+++ entrega1-6/__tests__/validaentrega1-6.test.js
@@ -1,4 +1,5 @@
-const operation = require('../app/entrega1-6-codi')
+const operation = require('../app/entrega1-6-codi.js')
 
+test("number sum", () => {expect(operation.sum([1, 1])).toBe(2)})
 test("letter sum", () => {expect(operation.sum([1, "a"])).toBe("1a")})
 
--- entrega1-6/app/entrega1-6-codi.js
+++ entrega1-6/app/entrega1-6-codi.js
@@ -11,4 +11,4 @@ const multiply = function multiply(validatedArray) {const multiply = validatedAr
 const divide = function divide(validatedArray) {const divide = validatedArray.reduce((previousValue, currentValue) => previousValue / currentValue)
     return console.log(divide)}
 
-module.exports = {sum, substract, multiply, divide}
+module.exports = sum
