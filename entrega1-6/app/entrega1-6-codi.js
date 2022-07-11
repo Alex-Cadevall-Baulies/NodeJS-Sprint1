a1d8bf8efa6796c4eb4754411d619ab8ae69b011
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 12:45:26 +0200

    validating arrays

--- entrega1-6/app/entrega1-6-codi.js
+++ entrega1-6/app/entrega1-6-codi.js
@@ -1,4 +1,6 @@
-function sum(validatedArray) 
+
+function sum(validatedArray)
+validateArray (validatedArray)
 {const sum = validatedArray.reduce((previousValue, currentValue) => previousValue + currentValue)
 return sum}
 
@@ -10,5 +12,24 @@ function multiply(validatedArray) {const multiply = validatedArray.reduce((previ
 
 function divide(validatedArray) {const divide = validatedArray.reduce((previousValue, currentValue) => previousValue / currentValue)
     return divide}
+
+
+
+//funció per validar array
+function validateArray (validatedArray) {
+    const alphabet = "abcdefghijklmnopqrstuvwxyzç"
+    for (const letter of alphabet) {
+    if (numberArray.includes(letter)) {throw Error (`El array conté lletres`)}
+    }
+
+    let numberArrayLength = numberArray.length
+    if (numberArrayLength < 2) {throw Error (`El array conté menys de dos nombres`)}
+
+    if (numberArray.includes(null)) {throw Error (`El array conté un element Null`)}
+
+    if (!numberArray.includes(undefined)) {throw Error (`El array conté un element undefined`)}
+
+    return validatedArray
+}
 
 module.exports = {sum, substract, multiply, divide}
