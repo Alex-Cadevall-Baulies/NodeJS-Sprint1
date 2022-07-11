1329b0daf5acd93725b88371504f28b69a886f72
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 12:45:38 +0200

    validating arrays

--- entrega1-6/__tests__/validaentrega1-6.test.js
+++ entrega1-6/__tests__/validaentrega1-6.test.js
@@ -2,6 +2,6 @@ const {sum, substract, multiply, divide} = require('../app/entrega1-6-codi')
 
 describe ("Sum tests", () => {
 test("validate if sum works with numbers", () => {expect(sum([1, 1])).toBe(2)});
-test("validate if sum rejects letters", () => {expect(sum([1, "a"])).toBe("1a")})
-test("validate if sum rejects special characters", () => {expect(sum([1, "/"])).toBe("1a")})
+test("validate if sum rejects letters", () => {expect(sum([1, "a"])).toThrow(`El array conté lletres`)})
+test("validate if sum rejects special characters", () => {expect(sum([1, "/"])).toBe("1/")})
 })
