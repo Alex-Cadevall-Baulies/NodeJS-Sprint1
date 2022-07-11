169de5c75453fa75de36f116aba2f94e6d13ef6c
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 09:49:53 +0200

    validation changes included array more than 2

--- entrega1-6/__tests__/entrega1-6test.js
+++ entrega1-6/__tests__/entrega1-6test.js
@@ -7,13 +7,17 @@ function validateArray (numberArray) {
     if (!numberArray.toLowerCase().includes(letter)) {noLetters = true}
     }
 
+    let moreThanOneNumber = false
+    let numberArrayLength = numberArray.length
+    if (numberArrayLength >= 1) {moreThanOneNumber = true}
+
     let noNull = false
     if (!numberArray.includes(null)) {noNull = true}
 
     let noUndefined = false 
     if (!numberArray.includes(undefined)) {noUndefined = true}
 
-    const validArray = noLetters && noNull && noUndefined
+    const validArray = noLetters && moreThanOneNumber && noNull && noUndefined
     return validArray
 }
 
