029ba076be2666b337769814e5cd486d240fbc26
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 09:46:52 +0200

    validation

--- entrega1-6/__tests__/entrega1-6test.js
+++ entrega1-6/__tests__/entrega1-6test.js
@@ -1,5 +1,20 @@
-function validateArray (numberArray) {
+const { isNumber } = require("util")
 
+function validateArray (numberArray) {
+    let noLetters = false
+    const alphabet = "abcdefghijklmnopqrstuvwxyzç"
+    for (const letter of alphabet) {
+    if (!numberArray.toLowerCase().includes(letter)) {noLetters = true}
+    }
+
+    let noNull = false
+    if (!numberArray.includes(null)) {noNull = true}
+
+    let noUndefined = false 
+    if (!numberArray.includes(undefined)) {noUndefined = true}
+
+    const validArray = noLetters && noNull && noUndefined
     return validArray
 }
+
 module.exports = validateArray
