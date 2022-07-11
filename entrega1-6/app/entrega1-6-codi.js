4a682edd732695df16bb9ef10eb99e259f4ac490
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-11 13:04:03 +0200

    throw error changes

--- entrega1-6/app/entrega1-6-codi.js
+++ entrega1-6/app/entrega1-6-codi.js
@@ -1,7 +1,7 @@
 
-function sum(validatedArray)
-validateArray (validatedArray)
-{const sum = validatedArray.reduce((previousValue, currentValue) => previousValue + currentValue)
+function sum(array){
+const validatedArray = validateArray (array)
+const sum = validatedArray.reduce((previousValue, currentValue) => previousValue + currentValue)
 return sum}
 
 function substract(validatedArray) {const substract = validatedArray.reduce((previousValue, currentValue) => previousValue - currentValue)
@@ -13,23 +13,21 @@ function multiply(validatedArray) {const multiply = validatedArray.reduce((previ
 function divide(validatedArray) {const divide = validatedArray.reduce((previousValue, currentValue) => previousValue / currentValue)
     return divide}
 
+//codi per exportar
+module.exports = {sum, substract, multiply, divide}
 
-
-//funció per validar array
-function validateArray (validatedArray) {
+//funció per validar el array
+function validateArray (array) {
     const alphabet = "abcdefghijklmnopqrstuvwxyzç"
     for (const letter of alphabet) {
-    if (numberArray.includes(letter)) {throw Error (`El array conté lletres`)}
+        if (array.includes(letter)) {throw new Error (`El array conté lletres`)}
     }
-
-    let numberArrayLength = numberArray.length
-    if (numberArrayLength < 2) {throw Error (`El array conté menys de dos nombres`)}
-
-    if (numberArray.includes(null)) {throw Error (`El array conté un element Null`)}
-
-    if (!numberArray.includes(undefined)) {throw Error (`El array conté un element undefined`)}
-
-    return validatedArray
+    let arrayLength = array.length
+    if (arrayLength < 2) {throw new Error (`El array conté menys de dos nombres`)}
+    
+    if (array.includes(null)) {throw new Error (`El array conté un element Null`)}
+    
+    if (array.includes(undefined)) {throw new Error (`El array conté un element undefined`)}
+    
+    return array
 }
-
-module.exports = {sum, substract, multiply, divide}
