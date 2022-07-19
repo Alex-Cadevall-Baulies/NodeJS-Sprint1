9117a8bfaff7430115af64ddc6c7c56192da2786
Alex Cadevall Baulies <alexcadevall@gmail.com>
2022-07-19 12:14:25 +0200

    t

--- entrega1-6/__tests__/validaentrega1-6.test.js
+++ entrega1-6/__tests__/validaentrega1-6.test.js
@@ -61,6 +61,68 @@ describe("multiply tests", () => {
  */
 
 
-describe("name & salary test", () => {
-test("validate if correct ID resolve works properly", () => {return namePlusSalary(2).then(data => {expect(data).toBe(`El ID 2 correspon a Bill Gates amb salari 1000`)})
-})})
+ describe("name & salary test", () => {
+    test("validate if correct ID resolve works properly", async () => {expect(namePlusSalary(2)).toBeInstanceOf(Promise);
+    return expect( await namePlusSalary(2)).resolves.toEqual(`El ID 2 correspon a Bill Gates amb salari 1000`)})
+    })
+
+test("validate if correct ID resolve works properly", async () => {delayedResolveFunction = jest.fn();
+await namePlusSalary(delayedResolveFunction);  expect(delayedResolveFunction.mock.calls.length).toBe(1)})
+
+
+/* 
+var { delayedResolve, delayedExec } = require('../app/e4n1e2');
+
+jest.setTimeout(10000); //Pq no em salti el limit de timeout
+
+// console.log(`${jest.isMockFunction(delayedExec)} ${jest.isMockFunction(delayedResolve)}`);
+describe(`Comprova que fa tot el que demana el enunciat`, () => {
+    test(`Comprova que la asíncrona (delayedExec) crida a l'altra`, async () => {
+        delayedResolveMock = jest.fn();
+        await delayedExec(delayedResolveMock);
+        // expect(delayedResolve).toHaveBeenCalledTimes(1);
+        expect(delayedResolveMock.mock.calls.length).toBe(1);
+        //Pq .calls.length es 0 :( 
+        //ho era pq per molt que faci la assignacio delayedResolve = jest.fn(); aquí, quan s'executa el delayedExec crida al 
+        //delayedResolve d'allà, que no es una mock. Per solucionar-ho fem que delayedExec rebi delayedResolve com a callback    
+    });
+    test(`Comprova que l'altra (delayedResolve) torna una Promise, i que la promise es resol amb el valor esperat`, () => {
+        expect(delayedResolve()).toBeInstanceOf(Promise);
+        return expect(delayedResolve()).resolves.toBe(`M'he resolt!`);
+    });
+    test(`Comprova que la promise de delayedResolve triga mínim 2 seconds a resoldre's`, async () => {
+        jest.useFakeTimers();
+        delayedResolveMock = jest.fn(() => {
+            console.log(`Començo a esperar per resoldre'm...(Mock)`);
+            return new Promise((res, rej) => {
+                setTimeout(() => res(String(`M'he resolt!`)), 2000);
+                jest.advanceTimersByTime(2000);
+                // jest.advanceTimersByTime(1000); //Si comentes la linia de dalt i descomentes aquesta, el test dona timeout
+            });
+        });
+        return expect(delayedResolveMock()).resolves.toBe(`M'he resolt!`);
+    });
+});
+describe(`Comportaments si les crides de formes rares`, () => {
+    test(`Si la promesa falla, atrapa i mostra el message del Error`, async () => {
+        mockReject = jest.fn(() => {
+            return new Promise((res, rej) => {
+                if (!true)
+                    res(`Aixo no hauria de passar mai`);
+                else
+                    rej(new Error(`Sóc un error message`));
+            });
+        });
+        mockDelayedExec = jest.fn(async (fun) => {
+            try {
+                const str = await fun();
+                return(str);
+            }
+            catch (err) {
+                return (err);
+            }
+        });
+        expect(mockDelayedExec(mockReject)).resolves.toMatchObject(new Error(`Sóc un error message`)); //Utilitzo el resolve pq no ha llençat el error, l'ha retornat
+    });
+});
+*/
